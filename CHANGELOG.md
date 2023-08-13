917f98cb4ff0964c3d6e1f53a990027b5ae69889
MBRjun <neko@mbr.moe>
2023-08-13 19:03:16 +0800

    refactor: use TypeScript

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# v0.1.1
+- refactor: use TypeScript
+
 # v0.1.0
 - feat: now support Windows
 - refactor: add exec function
--- main.ts
+++ main.ts
@@ -1,29 +1,29 @@
-var nodeBot = require('node-telegram-bot-api');
-const { execFileSync } = require('child_process');
+import nodeBot from 'node-telegram-bot-api';
+import { execFileSync } from 'child_process';
 
 var token = process.env.TELEGRAM_TOKEN;
-var bot = new nodeBot(token, {polling: true});
+var bot = new nodeBot(token as string, {polling: true});
 
-function exec(path, args) {
+function exec(path: string, args: string[]) {
     console.log('\x1b[45m EXEC \x1b[0m ' + path + ' ARGS: ' + args );
     let stdout = execFileSync(path, args).toString();
     return stdout;
 }
 
 
-bot.onText(/[\s\S]*/, function (msg, resp) {
+bot.onText(/[\s\S]*/, function (msg: { chat: { id: any; }; }, resp) {
 var chatId = msg.chat.id;
     console.log('\x1b[44m RECV \x1b[42m ' + chatId + ' \x1b[0m ' + resp );
 });
 
-bot.onText(/\/status/, function (msg) {
+bot.onText(/\/status/, function (msg: { chat: { id: any; }; }) {
     var chatId = msg.chat.id;
     var resp = '*Kalium Bot*\n`' + exec('bash', ['neofetch', '--stdout']) + '`';
     console.log('\x1b[43m SEND \x1b[42m ' + chatId + ' \x1b[0m ' + resp );
     bot.sendMessage(chatId, resp,{ parse_mode: 'Markdown' });
 });
 
-bot.onText(/\/wol/, function (msg) {
+bot.onText(/\/wol/, function (msg: { chat: { id: any; }; }) {
     var chatId = msg.chat.id;
     const stdout = execFileSync('wakeonlan', ['08:bf:b8:43:30:15']);
     var resp = '`' + stdout.toString() + '`';
